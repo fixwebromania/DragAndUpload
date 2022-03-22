0494de2e02e054b2b5545797f01df6fa79d79a9c
fixwebromania <101620645+fixwebromania@users.noreply.github.com>
2022-03-22 06:51:41 +0200

    removed controller,implemented remove button,and filelist

--- admin/view/javascript/dragandupload.js
+++ admin/view/javascript/dragandupload.js
@@ -27,53 +27,71 @@ $(document).ready(function() {
     $('.upload-drop-zone').on('drop', function (e) {
         e.stopPropagation();
         e.preventDefault();
-
-		var table=$("#file-list");
+				
+		var files=e.originalEvent.dataTransfer.files;
+	
 		
-		table.html('');
-		
-		var html='';
-		
-        var files = e.originalEvent.dataTransfer.files;
-        var fd = new FormData();
 		
 		for(i=0;i<files.length;i++)
 		{
 			var file=files[i];
 			
-			html+='<tr class="paginate-show"><td><a href="#" target="_blank">'+file.name+'</a></td><td>October 31 2019, 11:52:25</td><td>38.25MB</td></tr>';
-			
-			fd.append('file', file);
+			uploadFile(file);
 		}
-		
-		table.html(html);
 	
-        uploadData(fd);
-    });
-
-    // file selected
-    $("#file").change(function(){
-        var fd = new FormData();
-
-        var files = $('#file')[0].files[0];
-
-        fd.append('file',files);
-
-        uploadData(fd);
     });
 });
 
-function uploadData(formdata){
+function uploadFile(file)
+{
+	var data = new FormData();
+
+	data.append('file',file);
+
+	sendData(data);
+}
+
+function sendData(formdata){
 
     $.ajax({
-        url: 'index.php?route=catalog/dragandupload&user_token='+user_token,
+        url: 'index.php?route=catalog/download/upload&user_token='+user_token,
         type: 'post',
         data: formdata,
         contentType: false,
         processData: false,
         dataType: 'json',
         success: function(response){
-            alert(response);
+            if(response.success)
+			{
+				addFile(response)
+			}
         }
     });
+}
+
+function deleteDownload(download_id)
+{
+	$("#product-download"+download_id).remove();
+	$("#du-"+download_id).remove();
+}
+
+function addFile(data)
+{
+	data['name']=data['mask'];
+	
+	data['download_description']={[drag_language_id]:{'name':data['mask']}};
+	
+	$.post('index.php?route=catalog/download/add&ajax=1&user_token='+user_token,data,function(r)
+	{
+		if(r.success)
+		{
+			var html='<div id="product-download'+r.download_id+'"><i class="fa fa-minus-circle"></i>'+data.name+'<input type="hidden" name="product_download[]" value="'+r.download_id+'"></div>';
+		
+			$("#product-download").append(html);
+			
+			html='<tr class="paginate-show" id="du-'+r.download_id+'"><td><a href="#" target="_blank">'+data.name+'</a></td><td></td><td><a href="javascript:;" onclick="deleteDownload('+r.download_id+')"><i class="fa fa-close"></i></a></td></tr>';
+					
+			$("#file-list").append(html);
+		}
+	})
 }
